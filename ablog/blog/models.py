39bb55f53507d804d49990ce405930cde2345045
Cdiazf <cdiazfarfan@gmail.com>
2021-12-20 19:02:36 -0500

    segundo  avance blog auth de blog

--- ablog/blog/models.py
+++ ablog/blog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    title_tag= models.CharField(max_length=100, default='basic blog')
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title + ' | ' + self.author.username
+
+    def get_absolute_url(self):
+        return reverse('article_details',args=[str(self.id)])
--- ablog/blog/views.py
+++ ablog/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
+from .forms import PostForm,EditForm
+
+
 # Create your views here.
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -14,3 +17,20 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
     context_object_name = 'post'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    #fields = ['title','title_tag','content']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = '/'
